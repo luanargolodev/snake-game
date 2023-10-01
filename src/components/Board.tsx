ef77848059095733030ed890d288bc2fc7fb8fd7
Luan Lemos <ipsluan@icloud.com>
2023-09-30 21:33:39 -0300

    feat: board

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,5 +1,41 @@
-import { View } from 'react-native'
+import { View, StyleSheet } from 'react-native'
+import { BORDER, BORDER_RADIUS, GAP, HEADER_HEIGHT, PIXEL } from '../consts'
+import { colors } from '../styles/theme'
 
-export default function Board() {
-  return <View></View>
+interface BoardProps {
+  rows: number
+  cols: number
+  top: number
 }
+
+export default function Board({ rows, cols, top }: BoardProps) {
+  const dots = Array(rows * cols).fill(0)
+
+  return (
+    <View style={[styles.board, { top: HEADER_HEIGHT + top }]}>
+      {dots.map((_, index) => (
+        <View style={styles.pixel} key={index}></View>
+      ))}
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  board: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    position: 'absolute',
+    marginHorizontal: BORDER,
+  },
+  pixel: {
+    width: PIXEL,
+    height: PIXEL,
+    borderColor: colors.p6,
+    borderWidth: GAP,
+    borderRadius: BORDER_RADIUS,
+    backgroundColor: colors.p5,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+})
--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,4 +1,8 @@
-import { PanGestureHandler } from 'react-native-gesture-handler'
+import {
+  PanGestureHandler,
+  PanGestureHandlerEventPayload,
+  PanGestureHandlerGestureEvent,
+} from 'react-native-gesture-handler'
 import { SafeAreaView, StyleSheet, Dimensions } from 'react-native'
 
 import Header from './Header'
@@ -7,12 +11,36 @@ import Snake from './Snake'
 import Food from './Food'
 import { colors } from '../styles/theme'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import { COLS, HEADER_HEIGHT, PIXEL } from '../consts'
+
+const { height } = Dimensions.get('window')
 
 export default function Game() {
   const insets = useSafeAreaInsets()
+  const ROWS = Math.floor(
+    (height - insets.top - insets.bottom - HEADER_HEIGHT) / PIXEL
+  )
+
+  function handleGesture(event: PanGestureHandlerGestureEvent) {
+    const { translationX, translationY } = event.nativeEvent
+
+    if (Math.abs(translationX) > Math.abs(translationY)) {
+      if (translationX > 0) {
+        console.log('right')
+      } else {
+        console.log('left')
+      }
+    } else {
+      if (translationY > 0) {
+        console.log('down')
+      } else {
+        console.log('up')
+      }
+    }
+  }
 
   return (
-    <PanGestureHandler>
+    <PanGestureHandler onGestureEvent={handleGesture}>
       <SafeAreaView style={styles.container}>
         <Header
           top={insets.top}
@@ -21,7 +49,7 @@ export default function Game() {
           pause={() => {}}
           reload={() => {}}
         />
-        <Board />
+        <Board top={insets.top} rows={ROWS} cols={COLS} />
         <Snake />
         <Food />
       </SafeAreaView>
